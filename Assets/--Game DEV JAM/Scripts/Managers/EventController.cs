6a6b034404412acd0668fe75a867b5f6a5e3162e
zatsujin <70454743+zatsujin@users.noreply.github.com>
2020-09-25 08:16:47 -0500

    -event manager scripts

---
+++ Assets/--Game DEV JAM/Scripts/Managers/EventController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventController : MonoBehaviour
+{
+    public EEventType eCurrentEvent;
+    public List<Interaction> objInteractions;
+    public List<Timer> objTimers;
+    public bool autoProgressDialogPhase;
+    private string _dynamicID;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _dynamicID = EventManager.Instance.AddDynamicObject(this.name, this.gameObject, this);
+    }
+
+    public void OnDialogCalled()
+    {
+        if (eCurrentEvent == EEventType.chat)
+            return;
+        EEventType prevEvent = eCurrentEvent;
+        eCurrentEvent = EEventType.chat;
+        EventManager.Instance.UpdateEvent(_dynamicID, prevEvent, eCurrentEvent);
+    }
+
+    public class Interaction
+    {
+        public enum EEventTrigger 
+        { 
+            onObjEnabled,
+            onObjDisabled,
+            onEnemyDefeated, 
+            onDialogPhaseShown, 
+            onInteracted,
+            onChagedEvent
+        };
+        public enum EEventAction 
+        { 
+            unlockNextDuel,
+            unlockNextDialog,
+            unlockNextDialogPhase,
+            disableSelfOffCameras,
+            enableSelfOffCameras,
+            changeEvent
+        };
+        public EventController objConnected;
+        public EEventTrigger trigger;
+        public EEventAction action;
+        //public EEventType eventTriggered;
+        //public EEventType eventToChange;
+
+        public void UpdateEventTrigger()
+        {
+
+        }
+
+    }
+
+    public class Timer
+    {
+        public enum EEventTimerAction
+        {
+            unlockNextDuel,
+            unlockNextDialog,
+            unlockNextDialogPhase,
+            disableSelfOffCameras,
+            enableSelfOffCameras,
+            changeEvent
+        };
+        public float timeInSecs;
+        public EEventTimerAction action;
+    }
+}
---
+++ Assets/--Game DEV JAM/Scripts/Managers/EventManager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EEventType { idle, battle, deafeated, chat};
+public class EventManager : Manager<EventManager>
+{
+    public int intsForDynamicIDs;
+    private Dictionary<string, DynamicObject> dic_dynamicObjects;
+    void Awake()
+    {
+        intsForDynamicIDs = -1;
+    }
+
+    public void UpdateEvent(string objID, EEventType prevEvent, EEventType nextEvent)
+    {
+        foreach (KeyValuePair<string, DynamicObject> obj in dic_dynamicObjects)
+        {
+
+        }
+    }
+
+    private void UnlockNextDuel()
+    {
+
+    }
+
+    private void UnlockNextDialog()
+    {
+
+    }
+
+    private void UnlockNextDialogPhase()
+    {
+
+    }
+
+    private void DisableOffCameras()
+    {
+
+    }
+
+    private void EnableOffCameras()
+    {
+
+    }
+
+    private void ChangeEvent(EEventType eventToSet)
+    {
+
+    }
+
+    public string AddDynamicObject(string name, GameObject obj, EventController controller)
+    {
+        string dynamicID;
+        int intId = DynamicID();
+        if (dic_dynamicObjects == null)
+            dic_dynamicObjects = new Dictionary<string, DynamicObject>();
+        dynamicID = name + intId.ToString();
+        dic_dynamicObjects.Add(dynamicID, new DynamicObject(obj, controller));
+        return dynamicID;
+    }
+
+    private int DynamicID()
+    {
+        intsForDynamicIDs++;
+        return intsForDynamicIDs;
+    }
+
+}
+
+public class DynamicObject
+{
+    GameObject obj;
+    EventController controller;
+
+    public DynamicObject (GameObject _obj, EventController _controller)
+    {
+        obj = _obj;
+        controller = _controller;
+    }
+}
