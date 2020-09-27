43ae64c7365b3e1b345373d3843b557fc7eaf8bd
Adrian <adrian.flores@karaokulta.com>
2020-09-27 16:59:19 -0500

    Flip player

--- Assets/GameDEVJAM/Scripts/PlayerController.cs
+++ Assets/GameDEVJAM/Scripts/PlayerController.cs
@@ -2,22 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Control : MonoBehaviour
-{ 
+public class PlayerController : MonoBehaviour
+{
     public float speed;
-    private Rigidbody rb; 
+    private Rigidbody rb;
     public Animator CameraAnim;
+    public SpriteRenderer characterSprite;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
+    private void Update()
+    {
+        
+    }
+
     void FixedUpdate()
     {
-        if (GameManager.Instance.CurrentGameState == GameManager.GameState.RUNNING) {
+        if (GameManager.Instance.CurrentGameState == GameManager.GameState.RUNNING)
+        {
             float moveHorizontal = Input.GetAxis("Horizontal");
             float moveVertical = Input.GetAxis("Vertical");
+
+            if (moveHorizontal > 0)
+                characterSprite.flipX = false;
+            else if (moveHorizontal < 0)
+                characterSprite.flipX = true;
+
             Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
             rb.velocity = movement * speed;
         }
